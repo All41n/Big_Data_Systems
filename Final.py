1359070f370e77cd49976a9bfacdadd94813768e
Allain <allainescobar@outlook.com>
2017-12-08 15:20:52 +0000

    predict/error/leastsqrs/totalsqr/sqrderror/rsqrd/sqrderrorgrad/coefficient functions added

--- Final.py
+++ Final.py
@@ -113,3 +113,35 @@ print("________________________________Covariance of eater and exam sets________
 print("\t\t\tCoveriance: ",covariance(easter_test,exam_grades))
 print("___________________________________________________________________________________________________________")
 print("________________________________Simple Linear Regression of eater and exam sets____________________________")
+def predict(alpha, beta, x):
+    return beta * x + alpha
+	
+def error(alpha, beta, x, y):
+    return y - predict(alpha, beta, x)
+	
+def sum_of_squared_errors1(alpha, beta, x, y):
+    return sum(error(alpha, beta, x_i, y_i) ** 2 for x_i, y_i in zip(x, y))
+	
+def least_squares_fit(x,y):
+    beta = correlation(x, y) * standard_deviation(y) / standard_deviation(x)
+    alpha = mean(y) - beta * mean(x)
+    return alpha, beta
+	
+def total_sum_of_squares(y):
+    return sum(v ** 2 for v in de_mean(y))
+	
+def r_squared(alpha, beta, x, y):
+    return 1.0 - (sum_of_squared_errors1(alpha, beta, x, y) / total_sum_of_squares(y))
+	
+def squared_error1(x_i, y_i, theta):
+    alpha, beta = theta
+    return error(alpha, beta, x_i, y_i) ** 2
+	
+def squared_error_gradient(x_i, y_i, theta):
+    alpha, beta = theta
+    return [-2 * error(alpha, beta, x_i, y_i),-2 * error(alpha, beta, x_i, y_i) * x_i] 
+	
+def coefficient(x,y):
+	b1 = covariance(x,y)/ variance(x)
+	b0 = mean(y) - b1 * mean(x)
+	return b1,b0
